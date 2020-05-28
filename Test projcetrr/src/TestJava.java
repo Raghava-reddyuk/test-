f5c6e6c0ad77bf0b91304b2f2d24c15b38433e1b
Raghava reddy <raghava.reddy@northgateps.com>
2020-05-28 17:11:42 +0530

    refs #1489 @5h30m

--- Test projcetrr/src/TestJava.java
+++ Test projcetrr/src/TestJava.java
@@ -6,7 +6,7 @@ public void test()
 	System.out.println("Integrate redmine");
 	System.out.println("Test");
 	System.out.println("Redmine Integration");
-	System.out.println("Redmine");
+	System.out.println("Adding the test method");
 }
 	
 }
